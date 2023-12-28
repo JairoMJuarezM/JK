e9b4a7d14cb299b0f6c41a069ac447c540c8173f
JairoMJuarezM <jairojuarez200@gmail.com>
2023-12-28 16:50:51 -0600

    Segundo Commit

---
+++ jkproyecto/src/config/theme.config.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider, createTheme } from "@mui/material";
+import React from "react";
+
+type ThemeProp = {
+    children: JSX.Element;
+}
+
+const theme = createTheme({});
+
+export const ThemeConfig: React.FC <ThemeProp> = ({children}) => {
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+
+}
